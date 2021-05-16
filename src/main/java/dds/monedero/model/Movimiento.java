4a0047a3257ea75d7b44e8e84493cc21099465e8
AlejandroDeheza <ale.deheza@gmail.com>
2021-05-16 16:46:20 -0300

    Cambio un detalle

--- src/main/java/dds/monedero/model/Movimiento.java
+++ src/main/java/dds/monedero/model/Movimiento.java
@@ -4,8 +4,8 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public abstract class Movimiento {
-  private LocalDate fecha;
-  private BigDecimal monto;
+  private final LocalDate fecha;
+  private final BigDecimal monto;
 
   public Movimiento(LocalDate fecha, BigDecimal monto) {
     this.fecha = fecha;
